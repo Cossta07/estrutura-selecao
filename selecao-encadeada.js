58878f39c43da7b6640cd580e5e6987164f1e8f7
Cossta07 <samuellinker46@gmail.com>
2024-03-27 16:02:44 -0400

    exemplo seleção encadeada

---
+++ selecao-encadeada.js
@@ -0,0 +1,9 @@
+let dia = "sabado";
+
+if (dia === "sabado ") {
+    console.log("descansar né!");
+} else if (dia === "domingo") {
+    console.log("Passeio em familia");
+} else {
+    console.log("Partiu sesc ")
+}
