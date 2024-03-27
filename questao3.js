f7e39fff1f7ebdefbae37b217a7dd238c47e7b65
Cossta07 <samuellinker46@gmail.com>
2024-03-27 16:24:35 -0400

    questao 3 feita

---
+++ questao3.js
@@ -0,0 +1,11 @@
+/*
+Como você pode usar uma declaração if para verificar se uma 
+variável é do tipo string em JavaScript?
+*/
+let freefire = "deitei um";
+
+if (typeof freefire === 'string') {
+    console.log('A variável é do tipo string.');
+} else {
+    console.log('A variável não é do tipo string.');
+}
