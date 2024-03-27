a6dcf1f27f5d3e04fa02253228d2474a564444e0
Cossta07 <samuellinker46@gmail.com>
2024-03-27 17:18:28 -0400

    questao 9 feita

--- questao9.js
+++ questao9.js
@@ -3,13 +3,16 @@ Como você pode usar uma declaração if, else if, else para
 classificar um triângulo em equilátero, isósceles ou escaleno com base em 
 seus lados em JavaScript?
 */
-var lado2 = 1;
-function Triangulo(lado1, lado2, lado3) {
+var lado1 = 5;
+var lado2 = 5;
+var lado3 = 5;
+
+function classificarTriangulo(lado1, lado2, lado3) {
     if (lado1 === lado2 && lado2 === lado3) {
         return "Equilátero"; 
     } else if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) {
-        return "isoceles" ;
+        return "Isósceles"; 
     } else {
         return "Escaleno"; 
     }
-    }
+}
